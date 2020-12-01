ccd6e0dce42a7560757cc5e3e2dedcaedcebb696
rawatanimesh <animesh.rawat20@gmail.com>
2020-12-01 22:19:36 +0530

    changes in tree-item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { TreeComponent } from './tree/tree.component';
 import { TreeItemComponent } from './tree/tree-item/tree-item.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, FlexLayoutModule, MatIconModule, MatButtonModule ],
   declarations: [ AppComponent, TreeComponent, TreeItemComponent ],
   bootstrap:    [ AppComponent ]
 })
