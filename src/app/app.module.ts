270373a91c931158a638916231e1d429bd8283f3
rawatanimesh <animesh.rawat20@gmail.com>
2020-12-01 19:55:35 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TreeComponent } from './tree/tree.component';
-import { TreeItemComponent } from './tree/tree-item/tree-item.component;
+import { TreeItemComponent } from './tree/tree-item/tree-item.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
--- src/app/tree/tree-item/tree-item.component.ts
+++ src/app/tree/tree-item/tree-item.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter  } from '@angular/core';
 
 @Component({
   selector: 'tree-item',
   templateUrl: './tree-item.component.html',
-  styleUrls: ['./tree-item.component.css']
+  styleUrls: ['./tree-item.component.scss']
 })
 export class TreeItemComponent implements OnInit {
 
+  @Input() treeList = [];
+  @Output() itemSelected = new EventEmitter();
   constructor() { }
 
   ngOnInit() {
+    // console.clear();
+    console.info('tree', this.treeList);
+  }
+
+  selectedItem(item) {
+    // if(item.selectable) return;
+    this.itemSelected.emit(item);
+    console.log('called here')
+  }
+
+  onCheckbox(event, item) {
+    console.log('event', event);
+    console.log('item', item)
+    item.checked = event;
+    item.event = 'checked';
+    this.selectedItem(item);
+    //if parent is selectable than select children as per parent
+    console.log('item', item)
   }
 
 }
